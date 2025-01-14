f035f732c85649b0a91a3166af6711eeda514d62
nousantx <nousantx@gmail.com>
2025-01-14 17:00:15 +0700

    feat: update layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-function App() {
+export default function App() {
   return (
-    <>
-      <h1 class="bg-red hover:bg-blue">Hello World Dancok</h1>
-    </>
+    <div className="w-100vw h-mn-100dvh bg-black text-white">
+      <main className="w-mx-1024px mx-auto">
+        <article className="h-100vh w-100% center flex-d-column">
+          <h1 className="fs-2rem fw-500 ls--0.015em lh-1">Hello World!</h1>
+          <p class="mt-12px fs-1rem fw-500 text-gray">
+            It's not the greatest, but it actually works!
+          </p>
+        </article>
+      </main>
+    </div>
   )
 }
-
-export default App
